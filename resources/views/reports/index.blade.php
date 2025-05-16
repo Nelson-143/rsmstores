f313e8ebd643b5a0527f609327c34f5f5d69db4a
Nelson-143 <welasonm1912@gmail.com>
2025-05-16 10:23:55 +0300

    fix zero error

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -283,11 +283,12 @@
                             <div class="col-md-4">
                                 <div class="text-muted">Avg. Order Value</div>
                                 <div class="h3">
-                                    @if(!empty($totalOrders) && $totalOrders > 0)
-                                        {{ number_format($incomeStatement['revenue']/$totalOrders, 2) }} {{ auth()->user()->account->currency }}
-                                    @else
-                                        0.00 {{ auth()->user()->account->currency }}
-                                    @endif
+                                   @if(!empty($totalOrders) && $totalOrders > 0)
+    {{ number_format($incomeStatement['revenue'] / max($totalOrders, 1), 2) }} {{ auth()->user()->account->currency }}
+@else
+    0.00 {{ auth()->user()->account->currency }}
+@endif
+
                                 </div>
                             </div>
                         </div>
